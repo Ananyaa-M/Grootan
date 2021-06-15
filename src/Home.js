a970c286c2c2b0ed4522a380c696197e30459919
Ananyaa-M <72242839+Ananyaa-M@users.noreply.github.com>
2021-06-15 13:16:48 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,8 @@
-import BlogList from './BlogList';
-import useFetch from './useFetch';
 const Home = () => {
-    const [blogs,ispending,error] = useFetch('http://localhost:8000/blogs');
     return ( 
         <center>
         <div className="home">
             <h1>Homepage</h1>
-            {ispending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {blogs && <BlogList blogs={blogs} />}
         </div></center>
      );
 }
